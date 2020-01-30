4ac1f85e7152fb1472cea9241ed720a99d8bf62f
Matthew Ross <matthew.douglas.ross@gmail.com>
2020-01-30 15:44:17 -0500

    wrote function for dropping a game piece..... not sure if it works, can't test yet

--- src/components/game/Board.js
+++ src/components/game/Board.js
@@ -1,13 +1,13 @@
 // actual game board --- contains 2d array comprising the "board", game logic, game pieces, etc.
 
 
-
-const spaceState = {
-    0: "empty",
-    1: "player1",
-    2: "player2"
-}
-Object.freeze(spaceState)
+// implementation of an enum --- but i don't think we need it
+// const spaceState = {
+//     0: "empty",
+//     1: "player1",
+//     2: "player2"
+// }
+// Object.freeze(spaceState)
 
 const gameGrid = [
     [0, 0, 0, 0, 0, 0, 0],
@@ -19,17 +19,29 @@ const gameGrid = [
 ]
 
 // on their turn player can drop a piece into a column --- unless column is full
+let turnNumber = 1
 
-// search the grid for win condition after each players turn 
+const whichPlayer = numTurns => {
+    if (numturns % 2 === 0) {
+        return 2
+    }
+    else {
+        return 1
+    }
+}
 
 const dropPiece = (columnIndex) => {
-    // check if the bottom row @ columnIndex === 0 ---- if so, change it's value to whatever player's turn it is
-    // if not, go to the one right above it and repeat the process  
+    // check the bottom of column. if empty, change to value of whatever player's turn it is
     for (i = gameGrid.length; i > 0; i--) {
         if (gameGrid[i - 1][columnIndex] === 0) {
             // change value at that coordinate
-            // break
+            gameGrid[i - 1][columnIndex] = whichPlayer(turnNumber)
+            break
         }
     }
-    // toggle to other players turn
+
+    turnNumber++
 }
+
+// search the grid for win condition after each players turn 
+
